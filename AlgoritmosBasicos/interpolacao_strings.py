2f69fa6e65337493bfa8975563b61cfe5b49b6e3
Victor Bertoldo <eu_viktor@hotmail.com>
2020-05-28 16:32:14 -0300

    Interpolação e identidade

---
+++ AlgoritmosBasicos/interpolacao_strings.py
@@ -0,0 +1,18 @@
+﻿nome, idade = 'Ana', 30
+
+# Vamos usar o %s e o %d
+# O %s para substituição de um valor string
+# O %d para substituição de valores inteiros
+# O %f para substituição de valores float
+#       - para trazer menos casas use %.2f
+# O %r para boolean
+print("Nome: %s, Idade: %d" % (nome, idade))
+
+
+# Format
+
+print("Nome: {0}, Idade: {1}".format(nome, idade))
+
+# Nova forma... Só funciona a partir do 3.6
+
+print(f'Nome: {nome}, Idade: {idade} {2 ** 8 + 1}')
