8e7f20443848c6bc8cb072d8cb63ac808ab9cc56
victor.bertoldo <eu_viktor@hotmail.com>
2021-03-23 19:11:17 -0300

    semana DS do Minerando

---
+++ minerando/elasticTeste.py
@@ -0,0 +1,21 @@
+﻿from elasticsearch import helpers, Elasticsearch
+import csv
+
+
+# Conexão ao Elastic
+host = '10.0.10.7:9200'
+es = Elasticsearch(
+    host,
+    http_auth=('elastic', 'EBkWmpXjVdwdaLtIv5wb'),
+    scheme="https",
+    port=443,
+    verify_certs=False
+    )
+
+# ler o arquivo csv e com helpers.bulk inserir
+with open('dados.csv', 'r') as dados:
+    leitor = csv.DictReader(dados)
+    res = helpers.bulk(es, leitor, index='chamados')
+    # imprimir resultado do comando helpers.bulk
+    print(len(res))
+    print(res[0:-1])
---
+++ minerando/teste.py
@@ -0,0 +1,17 @@
+﻿import pandas as pd
+import json
+
+csv = pd.read_csv('dados.csv')
+print(csv.head())
+
+json_var = dict(csv)
+
+for i, j in json_var.items():
+    print(i ,': ',j)
+    
+
+csv.to_json(orient='records', path_or_buf='json_.json')
+
+
+app_json = json.dumps(json_var)
+print(app_json)
